5bb5813120dcfd52e174a045e2f533a38e3c2c7f
FayWandera <faywandera@gmail.com>
2022-10-29 11:34:48 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - Finds the number of elements in a linked list_t list.
  *
- * @h: The linkde list_t list.
+ * @h: The linked list_t list.
  *
  * Return: The number of elements in h.
  */
@@ -11,10 +11,10 @@ size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		elements++;
 		h = h->next;
+		elements++;
 	}
 	return (elements);
 }
