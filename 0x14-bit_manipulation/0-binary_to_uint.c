bc01d63ed50f67ac898fa49cee4b430e2ca40931
FayWandera <faywandera@gmail.com>
2022-11-05 11:49:48 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary number to an unsigned int.
+ * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointer to a string containg a binary number
  * Return: unsinged int with decimal balue of binary number, or 0 if error
  */
