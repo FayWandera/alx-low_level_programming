01a062b9b9329b60886e2e6bcf3a4025ebba1cda
FayWandera <faywandera@gmail.com>
2023-08-17 14:28:42 -0700

    add task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> This is a function that prints uppercase alphabets
+ * @c: is an integer parameter
+ * return: upper case letters is successful
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
